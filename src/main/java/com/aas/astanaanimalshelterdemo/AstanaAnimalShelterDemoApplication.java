c4ed2b9408b4d62352a0f63cf421f513b504ab43
Namig1991 <safarovnamig09@gmail.com>
2022-08-22 11:39:06 +0300

    My commit#1. Create telegram bot for Astana animal shelter.

---
+++ src/main/java/com/aas/astanaanimalshelterdemo/AstanaAnimalShelterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aas.astanaanimalshelterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstanaAnimalShelterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstanaAnimalShelterDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aas/astanaanimalshelterdemo/AstanaAnimalShelterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aas.astanaanimalshelterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstanaAnimalShelterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
